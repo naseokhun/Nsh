2af1542dd108c042f3771f271948f3aaf76bd6e5
naseokhun <81973857+naseokhun@users.noreply.github.com>
2022-03-10 15:47:13 +0900

    채팅

--- third/third/third.cpp
+++ third/third/third.cpp
@@ -1,6 +1,65 @@
-﻿#include <iostream>
+﻿//#include <iostream>
+//#include <thread>
+//
+//using namespace std;
+//
+//void th_run1() 
+//{
+//	while (true)
+//	{
+//		cout << "이히히힣" << endl;
+//	}
+//}
+//
+//int main()
+//{
+//	thread* th = new thread(th_run1);
+//	th->join();
+//	return 0;
+//}
+#include <iostream>
+#include <thread>
+#include <chrono>
 
-int main()
+using namespace std;
+
+int trigger;
+
+void naseokhun() {
+	while (trigger == 0)
+	{
+		this_thread::sleep_for(std::chrono::microseconds(1000));
+		cout << "정석\n";
+		this_thread::sleep_for(std::chrono::microseconds(1000));
+	}
+	
+}
+
+void ohjeongseok() {
+	while (trigger == 0)
+	{
+		this_thread::sleep_for(std::chrono::microseconds(500));
+
+		cout << "석훈\n";
+
+		this_thread::sleep_for(std::chrono::microseconds(500));
+	}
+	
+}
+
+int main() 
 {
-    std::cout << "Hello World!\n";
+	trigger = 0;
+	cout << "어렵넿\n";
+	thread na(naseokhun);
+	thread oh(ohjeongseok);
+	while (trigger == 0)
+	{
+		cin >> trigger;
+	}
+	na.join();
+	oh.join();
+	cout << "진짜어렵네\n";
+
+
 }
